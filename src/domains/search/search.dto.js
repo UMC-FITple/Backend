84e95e9045b26e2f3de0ee7cbdfa11aaf8ab474a
seoyeoneel02 <seoyeoneel02@gmail.com>
2024-08-06 21:23:59 +0900

    feat: 통합검색 조회 기능 구현 #19

---
+++ src/domains/search/search.dto.js
@@ -0,0 +1,123 @@
+// preview search response DTO
+export const previewSearchResponseDTO = (data) => {
+    
+    const cloth = [];
+
+    for (let i = 0; i < data.length; i++) {
+        cloth.push({
+            "nickname": data[i].nickname,
+            "brand": data[i].brand_name,
+            "cloth_name": data[i].name,
+            "size": data[i].size,
+            "fit": data[i].fit
+        })
+    }
+
+    console.log("dto", cloth);
+
+    return {"clothData": cloth, "cursorId": data[data.length-1].id};
+}
+
+// preview cloth response DTO
+export const previewClothResponseDTO = (data, userData) => {
+    
+    const cloth = [];
+    const user = [];
+    const fit = [];
+    const style = [];
+
+    cloth.push({
+        "brand": data[0][0].brand_name,
+        "cloth_name": data[0][0].name,
+        "product_code": data[0][0].product_code,
+        "size": data[0][0].size,
+        "fit": data[0][0].fit,
+        "color": data[0][0].color,
+        "URL": data[0][0].url,
+        "memo": data[0][0].memo
+    })
+    user.push({
+        "nickname": userData[0][0][0].nickname,
+        "height": userData[0][0][0].height,
+        "weight": userData[0][0][0].weight
+    })
+    for (let i = 0; i < userData[1][0].length; i++) {
+        fit.push(userData[1][0][i].pf_name);
+    }
+    for (let i = 0; i < userData[2][0].length; i++) {
+        style.push(userData[2][0][i].style_name);
+    }
+
+    return {"clothData": cloth, "userData": user, "prefer_fit": fit, "prefer_style": style};
+}
+
+// search result response DTO
+export const SearchResultResponseDTO = (clothData, brandData, userData) => {
+    
+    const cloth = [];
+    const brand = [];
+    const user = [];
+
+    if(clothData.length == 0){
+        cloth.push("해당 제품은 등록되어 있지 않아요.");
+    }else{
+        for (let i = 0; i < clothData.length; i++) {
+            cloth.push({
+                "nickname": clothData[i].nickname,
+                "brand": clothData[i].brand_name,
+                "cloth_name": clothData[i].name,
+                "size": clothData[i].size,
+                "fit": clothData[i].fit
+            })
+        }
+    }console.log(cloth)
+
+    if(brandData.length == 0){
+        brand.push("해당 브랜드는 등록되어 있지 않아요.");
+    }else{
+        for (let i = 0; i < brandData.length; i++) {
+            brand.push({
+                "brand_id": brandData[i].id,
+                "brand_name": brandData[i].name,
+                "eng_name": brandData[i].eng_name
+            })
+        }
+    }console.log(brand)
+
+    if(userData.length == 0){
+        user.push("해당 유저는 등록되어 있지 않아요.")
+    }else{
+        for (let i = 0; i < userData.length; i++) {
+            user.push({
+                "user_id": userData[i].user[0].uuid,
+                "nickname": userData[i].user[0].nickname,
+                "height": userData[i].user[0].height,
+                "weight": userData[i].user[0].weight,
+                "prefer_fit": userData[i].fit.map(fitItem => fitItem.pf_name),
+                "prefer_style": userData[i].style.map(styleItem => styleItem.style_name)
+            })
+        }
+    }
+    return {"clothData": cloth, "brandData": brand, "userData": user};
+}
+
+// search result response DTO
+export const SearchBrandResponseDTO = (brand, clothData) => {
+    
+    const cloth = [];
+
+    if(clothData.length == 0){
+        cloth.push("해당 제품은 등록되어 있지 않아요.");
+    }else{
+        for (let i = 0; i < clothData.length; i++) {
+            cloth.push({
+                "nickname": clothData[i].nickname,
+                "brand": clothData[i].brand_name,
+                "cloth_name": clothData[i].name,
+                "size": clothData[i].size,
+                "fit": clothData[i].fit
+            })
+        }
+    }
+    return {"brand_name": brand[0].name, "eng_name": brand[0].eng_name, "clothData": cloth};
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-
 import SwaggerUi from 'swagger-ui-express';
 import { specs } from './swagger/swagger.config.js';
 import { init } from './db/index.js';
-import { signupRouter } from './routes/signup.js';
 import { status } from './config/response.status.js';
 import { BaseError } from './config/error.js';
 import { response } from './config/response.js';
+import { signupRouter } from './routes/signup.js';
+import { searchRouter } from './routes/search.js';
 
 const app = express();
 const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('환영합니다 핏플 백엔드!');
+});
 
 await init();
 
@@ -21,10 +25,14 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
+// swagger
 app.use('/api-docs', SwaggerUi.serve, SwaggerUi.setup(specs));
 
+// router setting
 app.use("/FITple/signup",signupRouter);
+app.use('/FITple/search', searchRouter);
 
+// error handling
 app.use((req, res, next) => {
   const err = new BaseError(status.NOT_FOUND);
   next(err);
@@ -33,7 +41,7 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
   res.locals.message = err.message;
   res.locals.error = process.env.NODE_ENV !== 'production' ? err : {}; 
-  res.status(err.data.status).send(response(err.data));
+  res.status(err.data.status || status.INTERNAL_SERVER_ERROR).send(response(err.data));
 });
 
 app.listen(port, () => {
