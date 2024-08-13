bcd0c5ee15539cee5db75e19b7e4484fbbec46ee
ChoiGeon <qrwrqrwrer@naver.com>
2024-08-13 22:32:03 +0900

    feat: 아이디 찾기 및 비밀번호 재설정 기능 완료

--- src/config/response.status.js
+++ src/config/response.status.js
@@ -32,4 +32,18 @@ export const status = {
   // refreshToken err
   REFRESH_TOKEN_NOT_PROVIDED: { status: StatusCodes.UNAUTHORIZED, isSuccess: false, code: "REFRESHTOKEN001", message: "refresh 토큰이 제공되지 않았습니다." },
   REFRESH_TOKEN_INVALID: { status: StatusCodes.PAYMENT_REQUIRED, isSuccess: false, code: "REFRESHTOKEN002", message: "유효하지 않은 refresh 토큰입니다." },
+
+  // find-id err
+  FIND_ID_EMPTY_DATA: { status: StatusCodes.UNAUTHORIZED, isSuccess: false, code: "FINDID001", message: "데이터가 비어있습니다." },
+  FIND_ID_USER_NOT_FOUND: { status: StatusCodes.PAYMENT_REQUIRED, isSuccess: false, code: "FINDID002", message: "존재하지 않는 사용자입니다." },
+
+  // auth-reset-password err
+  AUTH_RESET_PASSWORD_EMPTY_DATA: { status: StatusCodes.UNAUTHORIZED, isSuccess: false, code: "AUTHRESETPASSWORD001", message: "데이터가 비어있습니다." },
+  AUTH_RESET_PASSWORD_USER_NOT_FOUND: { status: StatusCodes.PAYMENT_REQUIRED, isSuccess: false, code: "AUTHRESETPASSWORD002", message: "존재하지 않는 사용자입니다." },
+
+  // set-reset-password err
+  SET_RESET_PASSWORD_EMPTY_TOKEN: { status: StatusCodes.UNAUTHORIZED, isSuccess: false, code: "SETRESETPASSWORD001", message: "비밀번호 변경 권한 토큰이 존재하지 않습니다." },
+  SET_RESET_PASSWORD_EMPTY_NEW_PASSWORD: { status: StatusCodes.PAYMENT_REQUIRED, isSuccess: false, code: "SETRESETPASSWORD002", message: "변경할 비밀번호 데이터가 비어있습니다." },
+  SET_RESET_PASSWORD_CHANGE_PASSWORD_ERROR: { status: StatusCodes.FORBIDDEN, isSuccess: false, code: "SETRESETPASSWORD003", message: "비밀번호 변경에 실패했습니다." },
+  SET_RESET_PASSWORD_VERIFY_TOKEN_ERROR: { status: StatusCodes.NOT_ACCEPTABLE, isSuccess: false, code: "SETRESETPASSWORD004", message: "비밀번호 변경 권한 토큰이 유효하지 않습니다." },
 };
