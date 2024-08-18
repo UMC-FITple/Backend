e1385f0f7c242bb6fe0e3cdfc7f1ec2f319a70fe
ChoiGeon <qrwrqrwrer@naver.com>
2024-08-19 00:54:03 +0900

    fix: 내 프로필 response status

--- src/domains/myprofile/myprofile.controller.js
+++ src/domains/myprofile/myprofile.controller.js
@@ -5,18 +5,18 @@ import { MyprofileSaveService } from "./myprofile.service.js";
 export async function MyprofileSaveLogic(req, res) {
     try {
         if(!req.file){
-            return res.send(response(status.MYPROFILE_EMPTY_IMG));
+            return res.status(401).send(response(status.MYPROFILE_EMPTY_IMG));
         }
         const img_url = req.file.location;
 
         if(!req.body.data){
-            return res.send(response(status.MYPROFILE_EMPTY_DATA));
+            return res.status(402).send(response(status.MYPROFILE_EMPTY_DATA));
         }
         const jsonData = JSON.parse(req.body.data);
         const { userInfo, bodyInfo } = jsonData;
 
         if(!userInfo, !bodyInfo){
-            return res.send(response(status.MYPROFILE_EMPTY_DATA));
+            return res.status(402).send(response(status.MYPROFILE_EMPTY_DATA));
         }
 
         const uuid = res.locals.uuid;
@@ -24,27 +24,27 @@ export async function MyprofileSaveLogic(req, res) {
         const result = await MyprofileSaveService(userInfo,uuid,bodyInfo,img_url);
 
         if (result.code == 401) {
-            return res.send(response(status.MYPROFILE_EXIST_BODY_INFO));
+            return res.status(403).send(response(status.MYPROFILE_EXIST_BODY_INFO));
         }
         if(result.code == 402){
-            return res.send(response(status.MYPROFILE_BODY_INFO_SAVE_ERROR));
+            return res.status(404).send(response(status.MYPROFILE_BODY_INFO_SAVE_ERROR));
         }
         if(result.code == 403){
-            return res.send(response(status.MYPROFILE_USER_INFO_SAVE_ERROR));
+            return res.status(405).send(response(status.MYPROFILE_USER_INFO_SAVE_ERROR));
         }
         if(result.code == 404){
-            return res.send(response(status.MYPROFILE_USER_IMG_SAVE_ERROR));
+            return res.status(406).send(response(status.MYPROFILE_USER_IMG_SAVE_ERROR));
         }
         if(result.code == 405){
-            return res.send(response(status.MYPROFILE_EXIST_USER_NICKNAME));
+            return res.status(407).send(response(status.MYPROFILE_EXIST_USER_NICKNAME));
         }
         if(result.code === 500){
-            return res.send(response(status.INTERNAL_SERVER_ERROR));
+            return res.status(500).send(response(status.INTERNAL_SERVER_ERROR));
         }
 
         return res.send(response(status.SUCCESS));
     } catch (err) {
         console.error(err);
-        return res.send(response(status.INTERNAL_SERVER_ERROR));
+        return res.status(500).send(response(status.INTERNAL_SERVER_ERROR));
     }
 }
