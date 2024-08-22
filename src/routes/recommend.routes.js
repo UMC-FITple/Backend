0b0ce72b7b174b6407b03f2417fb351c22e32fc5
도리/이재욱 <hsh770217@gmail.com>
2024-08-23 04:43:24 +0900

    fix: 스웨거 파일 및 라우터 수정

--- src/routes/recommend.routes.js
+++ src/routes/recommend.routes.js
@@ -1,26 +1,11 @@
 // routes/recommend.route.js
 import express from "express";
 import asyncHandler from 'express-async-handler';
-import { get_bodyinfo_all, get_bodyinfo_by_uuid, get_member_by_uuid, get_userfit_by_uuid, get_userstyle_by_uuid, recommend_bodyinfo_by_uuid, recommend_style_by_uuid, train_bodyinfo, train_style } from "../domains/recommend/recommend.controller.js"
+import { recommend_bodyinfo_by_uuid, recommend_style_by_uuid, train_bodyinfo, train_style } from "../domains/recommend/recommend.controller.js"
 import { LoginCheck } from "../middlewares/logincheck.js";
 
 
 export const recommendRouter = express.Router({mergeParams: true});
-
-// 추천: 모든 사용자 체형정보 얻기
-recommendRouter.get('/getbodyinfo/all', asyncHandler(get_bodyinfo_all));
-
-// 추천: id로 정보 얻기
-recommendRouter.get('/getbodyinfo/', LoginCheck, asyncHandler(get_bodyinfo_by_uuid));
-
-// 추천: id로 정보 얻기
-recommendRouter.get('/getmember/', LoginCheck, asyncHandler(get_member_by_uuid));
-
-// 추천: id로 정보 얻기
-recommendRouter.get('/getuserstyle/', LoginCheck, asyncHandler(get_userstyle_by_uuid));
-
-// 추천: id로 정보 얻기
-recommendRouter.get('/getuserfit/', LoginCheck, asyncHandler(get_userfit_by_uuid));
 
 // // 추천: 비슷한 체형 사용자
 recommendRouter.get('/bodyinfo/', LoginCheck, asyncHandler(recommend_bodyinfo_by_uuid));
