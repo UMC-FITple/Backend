63c23fa68968eda6e0a1096934b415b55288cf12
seoyeoneel02 <seoyeoneel02@gmail.com>
2024-08-23 05:15:09 +0900

    feat: 좋아하는 옷 조회 #108

--- src/domains/profile/profile.dao.js
+++ src/domains/profile/profile.dao.js
@@ -1,7 +1,8 @@
 import { pool } from "../../config/db.config.js";
 import { BaseError } from "../../config/error.js";
 import { status } from "../../config/response.status.js";
-import { getUser, getFitToUserId, getStyleToUserId, getFollowerToUserId, getFollowingToUserId } from "./profile.sql.js";
+import { getUser, getFitToUserId, getStyleToUserId, getFollowerToUserId, getFollowingToUserId,
+    getMyFitSQL, getMyFitCategorySQL } from "./profile.sql.js";
 
 export const getUserDAO = async (userId) => {
     try {
@@ -56,6 +57,23 @@ export const getFollowDAO = async (userId) => {
 
         conn.release();
         return { follower, following };
+    } catch (err) {
+        throw new BaseError(status.PARAMETER_IS_WRONG);
+    }
+}
+
+export const getMyFitDAO = async (userId, category) => {
+    try {
+        const conn = await pool.getConnection();
+        if(typeof category == "undefined"){
+            const [data] = await pool.query(getMyFitSQL, userId);
+            conn.release();
+            return data;
+        }else{
+            const [data] = await pool.query(getMyFitCategorySQL, [userId, category]);
+            conn.release();
+            return data;
+        }
     } catch (err) {
         throw new BaseError(status.PARAMETER_IS_WRONG);
     }
--- src/domains/profile/profile.provider.js
+++ src/domains/profile/profile.provider.js
@@ -1,6 +1,11 @@
-import { getInfoDTO } from "./profile.dto.js"
-import { getUserDAO, getFitDAO, getStyleDAO, getFollowDAO } from "./profile.dao.js";
+import { getInfoDTO, getMyFitDTO } from "./profile.dto.js"
+import { getUserDAO, getFitDAO, getStyleDAO, getFollowDAO, getMyFitDAO } from "./profile.dao.js";
 
 export const getInfo = async (userId) => {
     return getInfoDTO(await getUserDAO(userId), await getFitDAO(userId), await getStyleDAO(userId), await getFollowDAO(userId));
+}
+
+export const getMyFit = async (userId, query) => {
+    const { category } = query;
+    return getMyFitDTO(await getMyFitDAO(userId, category));
 }
