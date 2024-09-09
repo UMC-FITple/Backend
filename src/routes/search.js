072f021feb3ef929bd09b2ec587a979cb0ca3f93
seoyeoneel02 <seoyeoneel02@gmail.com>
2024-09-09 16:56:33 +0900

    feat: 검색에 로그인 확인 기능 추가 #119

--- src/routes/search.js
+++ src/routes/search.js
@@ -1,17 +1,21 @@
 import express from "express";
 import asyncHandler from 'express-async-handler';
+import { LoginCheck } from "../middlewares/logincheck.js";
 import { searchPreview, clothView, searchView, brandView } from "../domains/search/search.controller.js";
 
 export const searchRouter = express.Router({mergeParams: true});
 
 //검색-메인화면
-searchRouter.get('/main', asyncHandler(searchPreview));
+searchRouter.get('/main', LoginCheck, asyncHandler(searchPreview));
 
 //검색-아이템 상세정보
-searchRouter.get('/:clothId', asyncHandler(clothView));
+searchRouter.get('/:clothId', LoginCheck, asyncHandler(clothView));
 
 //검색-통합 검색
-searchRouter.get('', asyncHandler(searchView));
+searchRouter.get('', LoginCheck, asyncHandler(searchView));
 
 //검색-브랜드
-searchRouter.get('/brand/:brandId', asyncHandler(brandView));
+searchRouter.get('/brand/:brandId', LoginCheck, asyncHandler(brandView));
+
+//검색-옷장에 추가
+searchRouter.get('/:clothId/add', LoginCheck, asyncHandler(clothView));
